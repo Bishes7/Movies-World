d8d87dd52a3cee61ee7a9621d23e16d311bc6f78
Bishes Adhikari <specialbishes9@gmail.com>
2025-01-04 16:13:45 +1100

    html added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <nav className=" py-2 text-warning text-center fixed-top   ">
+        <h2 className="fw-bold">Movie World</h2>
+      </nav>
+    </div>
+  );
+};
+
+export default Hero;
