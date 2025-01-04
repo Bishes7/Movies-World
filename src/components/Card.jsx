83c59bfc2cba0c9fd6c748f770e568d648fdf416
Bishes Adhikari <specialbishes9@gmail.com>
2025-01-05 00:37:38 +1100

    html and css added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const Card = () => {
-  return <div></div>;
+const Card = ({}) => {
+  const Poster = "https://omdbapi.com/src/poster.jpg";
+  return (
+    <div className="container movie-card-item ">
+      <div className="row  border rounded text-dark p-3  ">
+        <div className="col-md">
+          <img src={Poster} alt="" />
+        </div>
+        <div className="col-md">
+          <h3>Movie Name</h3>
+          <p>IMDB Rating 8</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <div className="d-flex justify-content-around">
+            <button className="btn btn-info">Drama</button>
+            <button className="btn btn-danger">Action</button>
+          </div>
+          <div className="d-grid my-2">
+            <button className="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import Card from "./Card";
 
 const Hero = () => {
+  const movieStyle = {
+    backgroundImage: `url("https://omdbapi.com/src/poster.jpg")`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  };
   return (
     <div>
-      <nav className=" py-2 text-warning text-center fixed-top   ">
+      <nav className=" py-2 text-warning text-center fixed-top    ">
         <h2 className="fw-bold">Movie World</h2>
       </nav>
+
+      <div
+        className="hero d-flex justify-content-center text-white align-items-center  "
+        style={movieStyle}
+      >
+        <div className="hero-content">
+          <div className="form-center">
+            <div className="text-center">
+              <h1>Search Millions of Movies</h1>
+              <p>Find about the move more in details before watching them</p>
+            </div>
+          </div>
+
+          <div className="input-group my-5">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <button className="btn btn-danger" type="button" id="button-addon2">
+              Search
+            </button>
+          </div>
+
+          <div className="movie-card-display">
+            <Card />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
